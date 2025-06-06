2f100393ef458f1b87825fde59a28ad2a87bb793
ThetaOmega01 <zjhztjf@gmail.com>
2025-06-06 10:00:24 +0800

    - Update comment

--- roll.cpp
+++ roll.cpp
@@ -5,7 +5,8 @@
 
 /*
  * @brief Randomly pick a number between lowerLimit and uperLimit inclusive.
- * @param n The upper limit (exclusive) for the random number generation
+ * @param size_t lowerLimit The lower limit of the range.
+ * @param size_t upperLimit The upper limit of the range.
  * @return A random number between lowerLimit and upperLimit inclusive.
  */
 using std::string;
@@ -17,7 +18,7 @@ size_t randomPick(const size_t lowerLimit, const size_t upperLimit) {
   return dis(gen);
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
   std::vector<std::string> args(argv, argv + argc);
   const size_t rollStart{1};
   const size_t rollEnd{args.size() - 1};
