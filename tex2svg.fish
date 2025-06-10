0d0dbce487b0c30c34a70465267cbc56ff7e7860
ThetaOmega01 <zjhztjf@gmail.com>
2025-06-10 22:05:13 +0800

    - tex2svg

---
+++ tex2svg.fish
@@ -0,0 +1,36 @@
+#!/usr/bin/env fish
+
+# Usage: ./tex2svg.fish path/to/file.tex
+
+# Ensure exactly one argument
+if test (count $argv) -ne 1
+    echo "Usage: $argv[0] path/to/file.tex"
+    exit 1
+end
+
+set texfile $argv[1]
+
+set base (basename $texfile .tex)
+set outdir $base-svgs
+
+mkdir -p $outdir
+
+set tmpdir (mktemp -d)
+
+echo "Compiling $texfile to PDF with Tectonic ..."
+tectonic -X compile --outdir=$tmpdir $texfile
+
+set pdffile $tmpdir/$base.pdf
+if not test -f $pdffile
+    echo "Error: PDF not found at $pdffile"
+    exit 2
+end
+
+echo "Converting PDF to SVGs with pdf2svg..."
+# This will generate files like output-01.svg, output-02.svg, ...
+pdf2svg $pdffile $outdir/$base-%02d.svg all
+
+# Clean up temporary directory
+rm -rf $tmpdir
+
+echo "Done! SVG files are in: $outdir"
